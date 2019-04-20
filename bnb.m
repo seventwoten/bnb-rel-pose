5c49ad0fcf7278ba8454ef61da39994c2f65275c
seventwoten <a0085039@gmail.com>
2019-04-20 21:44:54 +0800

    Move bounds computation from bnb.m to cube class

--- bnb.m
+++ bnb.m
@@ -7,9 +7,7 @@ function [solutions] = bnb(init_blk, p, q, thres_stop)
 %   The threshold shrinks until thres_stop as Bnb progresses.)
 
     % Set up variables
-    Np = size(p, 1);
-    Nq = size(q, 1);
-    max_matches = min(Np, Nq);
+    max_matches = min(size(p, 1), size(q, 1));
 
     queue = cell(1,max_matches); % Set up one block queue per no. of matches 
     queue{max_matches} = init_blk.subdivide(); 
@@ -17,13 +15,6 @@ function [solutions] = bnb(init_blk, p, q, thres_stop)
     e_max = 0;
     i = max_matches;
     solutions = [];
-
-    % Prepare capacity matrix
-    n_C = 1 + Np + Nq + 1;
-    C = zeros(n_C, n_C);
-    C(1, 2:Np+1) = 1; % set edges from s to p nodes
-    C(1+Np+1 : 1+Np+Nq, end) = 1; % set edges from q nodes to t
-    %imagesc(C)
     
     iter = 1;
     while ~isempty(i)
@@ -32,31 +23,17 @@ function [solutions] = bnb(init_blk, p, q, thres_stop)
         queue{i}(1) = [];
         
         % Given rotation represented by block centre, find angles(Rp, q) 
-        R  = aa2mat(blk.centre);
-        Rp = (R * p')';
-
-        a = angles(Rp, q);
+        blk = blk.setContext(p, q);
         
         fprintf("Iteration %d   sigma: %d pi e_max = %d\n", iter, blk.sigma/pi, e_max);
         
         % Compute block lower bound with stopping threshold
-        edges_tight = a < thres_stop;
-        C(2:Np+1, 1+Np+1 : 1+Np+Nq) = edges_tight;
-        G = digraph(C);
-        blk.LB = maxflow(G, 1, Np+Nq+2);
+        blk = blk.setLowerBound(thres_stop);
         fprintf("Lower bound: %d\n", blk.LB);
         
         % Compute block upper bound at sqrt(3)-sigma threshold
-        thres = sqrt(3) * blk.sigma;
-        if thres > thres_stop
-            edges = a < thres;
-            C(2:Np+1, 1+Np+1 : 1+Np+Nq) = edges;
-            G = digraph(C);
-            blk.UB = maxflow(G, 1, Np+Nq+2);
-            fprintf("Upper bound: %d \t", blk.UB);
-        else
-            blk.UB = blk.LB;
-        end
+        blk = blk.setUpperBound(thres_stop);
+        fprintf("Upper bound: %d \t", blk.UB);
         
         % Update e_max
         if blk.LB > e_max
@@ -72,12 +49,12 @@ function [solutions] = bnb(init_blk, p, q, thres_stop)
         
         % Discard this block, or subdivide/terminate
         if blk.UB > 0 && blk.UB >= e_max
-            if thres > thres_stop
+            if sqrt(3) * blk.sigma > thres_stop
                 fprintf("Continue!\n");
                 queue{blk.UB} = [queue{blk.UB} blk.subdivide()];
             else
                 fprintf("Solution at stopping resolution: [%d %d %d], score: %d-%d, sigma: %d\n", blk.centre, blk.LB, blk.UB, blk.sigma);
-                solutions = [solutions solution(blk.centre, blk.sigma, blk.LB, edges_tight)];
+                solutions = [solutions solution(blk.centre, blk.sigma, blk.LB, blk.edges_tight)];
             end
         else
             fprintf("Discard!\n");
--- cube.m
+++ cube.m
@@ -1,5 +1,11 @@
 classdef cube < block
     %CUBE 3D block
+    properties
+        angleMat
+        capacityMat
+        edges_tight
+    end
+    
     methods
         function obj = cube(c, s, p)
             %CUBE Construct an instance of this class
@@ -30,6 +36,66 @@ classdef cube < block
             end
             
         end
+        
+        function [obj] = setContext(obj, p, q)
+            %SETCONTEXT Necessary computation before checking bounds
+            R  = aa2mat(obj.centre);
+            Rp = (R * p')';
+            obj.angleMat = angles(Rp, q);
+        end
+        
+        function [obj] = setLowerBound(obj, thres_stop)
+            %SETLOWERBOUND Set lower bound at stopping threshold
+            assert(~isempty(obj.angleMat), 'Context was not set');
+            obj.edges_tight = obj.angleMat < thres_stop;
+            obj.LB = obj.getMaxBipartiteMatching(obj.edges_tight);
+        end
+        
+        function [obj] = setUpperBound(obj, thres_stop)
+            %SETUPPERBOUND Set upper bound at sqrt(3) * sigma threshold
+            assert(~isempty(obj.angleMat), 'Context was not set');
+            thres = sqrt(3) * obj.sigma;
+            if thres > thres_stop
+                edges = obj.angleMat < thres;
+                obj.UB = obj.getMaxBipartiteMatching(edges);
+            else
+                obj.UB = obj.LB;
+            end
+        end
+        
+        function [out] = getMaxBipartiteMatching(obj, edges)
+            %GETMAXBIPARTITEMATCHING Return size of max bipartite matching
+            Np = size(edges, 1);
+            Nq = size(edges, 2);
+            obj = obj.setCapacityMatrix(Np, Nq, edges);
+            
+            % Call maxflow to get max bipartite matching
+            G = digraph(obj.capacityMat);
+            out = maxflow(G, 1, Np+Nq+2);
+        end
+        
+        function [obj] = setCapacityMatrix(obj, Np, Nq, edges)
+            %SETCAPACITYMATRIX Set capacityMat attribute to template matrix
+            C = obj.getTemplateCapacityMatrix(Np, Nq);
+            C(2:Np+1, 1+Np+1 : 1+Np+Nq) = edges;
+            obj.capacityMat = C;
+        end
+        
+    end
+    
+    methods (Static)
+        function [out] = getTemplateCapacityMatrix(Np, Nq)
+            % Prepare shared template capacity matrix
+            persistent C;
+            if isempty(C)
+                n_C = 1 + Np + Nq + 1;
+                C = zeros(n_C, n_C);
+                C(1, 2:Np+1) = 1; % set edges from s to p nodes
+                C(1+Np+1 : 1+Np+Nq, end) = 1; % set edges from q nodes to t
+            end
+            out = C;
+        end
+      
     end
 end
 
