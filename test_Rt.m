a7c32ccf0977b9ca99fbf0cb7cb112b3f9ea03a8
seventwoten <a0085039@gmail.com>
2019-07-28 17:39:31 +0800

    Use Scene class in tests, change search range, minor edits

--- test_Rt.m
+++ test_Rt.m
@@ -7,79 +7,25 @@
 
 close all; clear all;
 
+load('scenes\s30.mat');
+scene = s30(1);
+
+p = scene.view2;
+q = scene.view1;
 known_corr = [1,1];    % set to [] for no known correspondences
 diary_corr = '1c_';    % set to [] to turn off diary name
 
-p = [-0.0596,  0.2702, 1.  ;
-      0.3342, -0.1944, 1.  ;
-      0.242 , -0.1989, 1.  ;
-     -0.1747, -0.2492, 1.  ;
-      0.3273,  0.3383, 1.  ;
-     -0.2507,  0.0308, 1.  ;
-      0.1016,  0.5406, 1.  ;
-     -0.5639, -0.2959, 1.  ;
-      0.3975, -0.0002, 1.  ;
-     -0.3473, -0.3172, 1.  ;
-     -0.3618,  0.2303, 1.  ;
-     -0.119 ,  0.2816, 1.  ;
-      0.1135, -0.0818, 1.  ;
-      0.1882,  0.0011, 1.  ;
-     -0.7126, -0.3211, 1.  ;
-     -0.0104,  0.3141, 1.  ;
-     -0.4788, -0.0284, 1.  ;
-      0.8599, -0.6003, 1.  ;
-     -0.0263,  0.8015, 1.  ;
-      0.184 , -0.0701, 1.  ;
-      0.0721,  0.3367, 1.  ;
-     -0.1654,  0.44  , 1.  ;
-      0.1912, -0.1834, 1.  ;
-     -0.1941,  0.0929, 1.  ;
-      0.6122, -0.1875, 1.  ;
-      0.3611,  0.1535, 1.  ;
-     -0.3344, -0.2078, 1.  ;
-     -0.3695,  0.2464, 1.  ;
-     -0.0065,  0.3912, 1.  ;
-      0.3296, -0.1865, 1.  ];
-
-q = [-0.1439,  0.2391, 1. ;
-      0.2782, -0.241 , 1. ;
-      0.2164, -0.22  , 1. ;
-     -0.1804, -0.3419, 1. ;
-      0.1984,  0.347 , 1. ;
-     -0.2905, -0.0622, 1. ;
-     -0.0908,  0.5028, 1. ;
-     -0.6058, -0.5603, 1. ;
-      0.3073, -0.0367, 1. ;
-     -0.3319, -0.4418, 1. ;
-     -0.5738, -0.0664, 1. ;
-     -0.2083,  0.2329, 1. ;
-      0.0681, -0.1252, 1. ;
-      0.143 , -0.007 , 1. ;
-     -0.7273, -0.6051, 1. ;
-     -0.1084,  0.2911, 1. ;
-     -0.5479, -0.2438, 1. ;
-      0.8283, -0.643 , 1. ;
-     -0.3766,  0.6421, 1. ;
-      0.1386, -0.0976, 1. ;
-     -0.0261,  0.3398, 1. ;
-     -0.3094,  0.3628, 1. ;
-      0.1729, -0.1999, 1. ;
-     -0.2517,  0.0087, 1. ;
-      0.472 , -0.3153, 1. ;
-      0.3021,  0.1981, 1. ;
-     -0.3239, -0.3139, 1. ;
-     -0.4886,  0.0763, 1. ;
-     -0.1768,  0.3004, 1. ;
-      0.3177, -0.1731, 1. ];
-
-% Ground truth R and T in required input formats
-axis_angle = [-0.1095839, 0.089599, 0.2046666];
-t_long_lat = [-2.2142974, 1.5707963];  
-
-% Do R and T search, with R block and T patch centred at ground truth
 % Set input variables
-R_list       = [RCube([0,0,pi/4], pi/4), RCube([0,0,-pi/4], pi/4)];
-t_list       = [tPatch([pi,0], pi/2), tPatch([0,0], pi/2)];
+R_list       = [RCube([-3/8 * pi, -3/8 * pi, 0], pi/8), RCube([    -pi/8, -3/8 * pi, 0], pi/8), ...
+                RCube([-3/8 * pi,     -pi/8, 0], pi/8), RCube([    -pi/8,     -pi/8, 0], pi/8), ...
+                RCube([-3/8 * pi,      pi/8, 0], pi/8), RCube([    -pi/8,      pi/8, 0], pi/8), ...
+                RCube([-3/8 * pi,  3/8 * pi, 0], pi/8), RCube([    -pi/8,  3/8 * pi, 0], pi/8), ...
+                RCube([     pi/8, -3/8 * pi, 0], pi/8), RCube([ 3/8 * pi, -3/8 * pi, 0], pi/8), ...
+                RCube([     pi/8,     -pi/8, 0], pi/8), RCube([ 3/8 * pi,     -pi/8, 0], pi/8), ...
+                RCube([     pi/8,      pi/8, 0], pi/8), RCube([ 3/8 * pi,      pi/8, 0], pi/8), ...
+                RCube([     pi/8,  3/8 * pi, 0], pi/8), RCube([ 3/8 * pi,  3/8 * pi, 0], pi/8)];
+
+t_list       = [tPatch([0,pi/2], pi/2), tPatch([pi,pi/2], pi/2)];
 
 delta        = 0;             % minimum angular error in Rp and q 
 thres_stop_R = 1/32 * pi;     % Stop when cube diagonal drops below this value
@@ -89,13 +35,14 @@ epipole_thres = 0.7;          % Reject points that match more than this fraction
 
 diary([['diary\diary_Rt_', diary_corr],datestr(now,'yyyy-mm-dd','local'),'_',datestr(now,'hh.MM.ss','local'),'.txt'])
 diary on;
+
 fprintf("test_Rt: \n");
-fprintf("delta         =  %f \n",    delta);
+fprintf("delta         =  %f \n",   delta);
 fprintf("R_half_len    = %s pi \n", rats(R_list(1).sigma/pi,5));
 fprintf("thres_stop_R  = %s pi \n", rats(thres_stop_R/pi,5));
 fprintf("t_half_len    = %s pi \n", rats(t_list(1).sigma/pi,5));
 fprintf("thres_stop_t  = %s pi \n", rats(thres_stop_t/pi,5));
-fprintf("epipole_thres =  %f \n\n",  epipole_thres);
+fprintf("epipole_thres =  %f \n\n", epipole_thres);
 
 tic;
 
@@ -109,15 +56,14 @@ fprintf("Displaying up to 5:\n");
 
 num_sol = size(solutions,2);
 for s = 1: min(num_sol, 5)
-    fprintf("Solution %d: [%d %d %d], sigma: %s pi, score: %d rpy: [%d %d %d]\n", s, solutions(s).centre, rats(solutions(s).sigma/pi,5), solutions(s).LB, RCube.R2rpy(solutions(s).aa2mat()));
+    fprintf("Solution %d: [%f %f %f], sigma: %s pi, score: %d rpy: [%f %f %f]\n", ...
+            s, solutions(s).centre, rats(solutions(s).sigma/pi,5), solutions(s).LB, ...
+            RCube.R2rpy(solutions(s).aa2mat()));
     % For each R, plot 1 solution matrix representing one possible t
     figure, imagesc(solutions(s).patches(1).edges_stop);
 end
 
-rpy_list = zeros(num_sol, 3);
-for s = 1: num_sol
-    rpy_list(s,:) = RCube.R2rpy(solutions(s).aa2mat());
-end
-fprintf("Average rpy: [%d %d %d] Ground truth: [%d %d %d]\n", sum(rpy_list, 1)/num_sol, RCube.R2rpy(RCube(axis_angle,[]).aa2mat()));
+fprintf("Ground truth: axis-angle = [%f %f %f], rpy = [%f %f %f], theta-phi = [%f %f]\n", ... 
+        scene.cam2_aa, scene.cam2_rpy, scene.cam2_tp);
 
 diary off;
--- test_t_only.m
+++ test_t_only.m
@@ -1,87 +1,30 @@
 % Test for t-only case 
-% Search is close to ground truth R and T
+% Search with knowledge of ground truth R
 % 30 3D points seen in both views, with quantisation noise and no outliers
 
-% Ground truth: 
-% rpy: [0.2,-0.1,0.1] -> axis-angle as 3d point in R ball: [-0.1095839, 0.089599, 0.2046666]
-% xyz: [-0.3,-0.4, 0] -> theta-phi as longitude-latitude: [-2.2142974, 1.5707963]
-
 close all; clear all;
-diary(['diary\diary_t_',datestr(now,'yyyy-mm-dd','local'),'_',datestr(now,'hh.MM.ss','local'),'.txt'])
+
+% Set scene variables
+load('scenes\s30.mat');
+
+for scene_num = 1:10
+scene = s30(scene_num);
+diary(['diary\diary_t_',num2str(scene.N),'.',num2str(scene_num),'_',datestr(now,'yyyy-mm-dd','local'),'_',datestr(now,'hh.MM.ss','local'),'.txt'])
 diary on;
 
-p = [-0.0596,  0.2702, 1.  ;
-      0.3342, -0.1944, 1.  ;
-      0.242 , -0.1989, 1.  ;
-     -0.1747, -0.2492, 1.  ;
-      0.3273,  0.3383, 1.  ;
-     -0.2507,  0.0308, 1.  ;
-      0.1016,  0.5406, 1.  ;
-     -0.5639, -0.2959, 1.  ;
-      0.3975, -0.0002, 1.  ;
-     -0.3473, -0.3172, 1.  ;
-     -0.3618,  0.2303, 1.  ;
-     -0.119 ,  0.2816, 1.  ;
-      0.1135, -0.0818, 1.  ;
-      0.1882,  0.0011, 1.  ;
-     -0.7126, -0.3211, 1.  ;
-     -0.0104,  0.3141, 1.  ;
-     -0.4788, -0.0284, 1.  ;
-      0.8599, -0.6003, 1.  ;
-     -0.0263,  0.8015, 1.  ;
-      0.184 , -0.0701, 1.  ;
-      0.0721,  0.3367, 1.  ;
-     -0.1654,  0.44  , 1.  ;
-      0.1912, -0.1834, 1.  ;
-     -0.1941,  0.0929, 1.  ;
-      0.6122, -0.1875, 1.  ;
-      0.3611,  0.1535, 1.  ;
-     -0.3344, -0.2078, 1.  ;
-     -0.3695,  0.2464, 1.  ;
-     -0.0065,  0.3912, 1.  ;
-      0.3296, -0.1865, 1.  ];
+p = scene.view2;
+q = scene.view1;
 
-q = [-0.1439,  0.2391, 1. ;
-      0.2782, -0.241 , 1. ;
-      0.2164, -0.22  , 1. ;
-     -0.1804, -0.3419, 1. ;
-      0.1984,  0.347 , 1. ;
-     -0.2905, -0.0622, 1. ;
-     -0.0908,  0.5028, 1. ;
-     -0.6058, -0.5603, 1. ;
-      0.3073, -0.0367, 1. ;
-     -0.3319, -0.4418, 1. ;
-     -0.5738, -0.0664, 1. ;
-     -0.2083,  0.2329, 1. ;
-      0.0681, -0.1252, 1. ;
-      0.143 , -0.007 , 1. ;
-     -0.7273, -0.6051, 1. ;
-     -0.1084,  0.2911, 1. ;
-     -0.5479, -0.2438, 1. ;
-      0.8283, -0.643 , 1. ;
-     -0.3766,  0.6421, 1. ;
-      0.1386, -0.0976, 1. ;
-     -0.0261,  0.3398, 1. ;
-     -0.3094,  0.3628, 1. ;
-      0.1729, -0.1999, 1. ;
-     -0.2517,  0.0087, 1. ;
-      0.472 , -0.3153, 1. ;
-      0.3021,  0.1981, 1. ;
-     -0.3239, -0.3139, 1. ;
-     -0.4886,  0.0763, 1. ;
-     -0.1768,  0.3004, 1. ;
-      0.3177, -0.1731, 1. ];
 
 % Ground truth R and T in required input formats
-axis_angle = [-0.1095839, 0.089599, 0.2046666];
-t_long_lat = [-2.2142974, 1.5707963];  
+axis_angle = scene.cam2_aa;
+t_long_lat = scene.cam2_tp; 
 
-tic;
 
 % Set threshold variables
 delta      = 0;             % minimum angular error in Rp and q 
 thres_stop = 1/512 * pi;     % stop at this patch size
-R_half_len = 1/64 * pi;     % find T given an R block of this size
+R_half_len = 1/256 * pi;     % find T given an R block of this size
 t_half_len = 1/4 * pi;      % search within an initial T patch of this size 
 
 % Do T search for a particular R block (centred at ground truth)
@@ -101,6 +44,7 @@ t_list = [tPatch([pi/4   , pi/4  ], t_half_len),...
           tPatch([3*pi/4 , 3*pi/4], t_half_len),...
           tPatch([-pi/4  , 3*pi/4], t_half_len),...
           tPatch([-3*pi/4, 3*pi/4], t_half_len)];
+tic;
 stT = StereoT(p, q, n1, n2, t_list, thres_stop, []);
 [stT, solutions] = stT.findSolutions(true, true);
 
@@ -109,9 +53,17 @@ toc;
 fprintf("Number of solutions: %d\n", size(solutions,2));
 fprintf("Displaying up to 5:\n");
 for s = 1: min(size(solutions,2), 5)
-    fprintf("Solution %d: [%d %d], sigma: %d pi, score: %d\n", s, solutions(s).centre, solutions(s).sigma/pi, solutions(s).LB);
+    t_err = StereoInterface.angles(solutions(s).centre_xyz, scene.cam2_xyz);
+    fprintf("Solution %d:  theta-phi = [%f %f], score: %d, t-error: %f (%f deg)\n", ...
+            s, solutions(s).centre, solutions(s).LB, t_err, rad2deg(t_err));
+    
     % Plot solution matrices
     figure, imagesc(solutions(s).edges_stop);
 end
 
+fprintf("Ground truth: aa = [%f %f %f], rpy = [%f %f %f], theta-phi = [%f %f]\n", ...
+        scene.cam2_aa, scene.cam2_rpy, scene.cam2_xyz);
+
 diary off;
+
+end
